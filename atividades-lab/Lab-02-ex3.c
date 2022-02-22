1cba209b0485323ca16185439145315de5663d2e
Gabriel Batista <gabriel.devproject@gmail.com>
2022-02-22 08:28:58 -0300

    Atualizando exercicios

--- atividades-lab/Lab-02-ex3.c
+++ atividades-lab/Lab-02-ex3.c
@@ -5,26 +5,22 @@
 int main() 
 {
     int input = 0;
-    char str[100], strFinal[100];
+    int *repo;
 
     while(input%2 == 0) {
         printf("Insira um número máximo Ímpar: ");
         scanf("%d", &input);
     }
 
-    for(int i=1; i>=input; i++) {
-        sprintf(str, "%d ", i);
-        strcat(strFinal, str);
+    repo = (int *)malloc(input * sizeof(int));
+
+    for(int i = 0; i <= input; i++){
+        repo[i] = i+1;
+    }
+    for(int i = 0; i < input; i++){
+        printf("%d ", repo[i]);
     }
 
-    printf("Valor: %s", strFinal);
-
-//     for(int j=0; j<input; j++){
-//         if(j>0){
-//            printf("\n%c", strFinal[j]);
-//         }
-
-//     }
-//  //   printf("Valor: %s", str);
-//     return 0;
+    free(repo);
+    return 0;
 }
