af90d24ac2fa8a8990757db7d5a0b450b51d2bfe
Fabio Tanikawa <vinifaro.fabio@gmail.com>
2022-02-23 21:25:41 -0300

    Atualizando os codigos

--- atividades-lab/Lab-02-ex8.c
+++ atividades-lab/Lab-02-ex8.c
@@ -8,3 +8,38 @@ void transposta(int a[LIM][LIM], int at[LIM][LIM], int m, int n) //recebe a matr
     for (i = 0; i < m; i = i + 1)
         for (j = 0; j < n; j = j + 1) at[j][i] = a[i][j];
 }
+int main(void)
+{
+    int i, j, m, n, mat[LIM][LIM], matTransp[LIM][LIM];
+    int cont = 1;
+    m=3;
+    n=3;    
+    printf("Imprimindo a matriz...\n");
+    if (m < LIM && n < LIM)
+    {
+        for (i = 0; i < 3; i = i + 1)
+        {        
+            for (j = 0; j < 3; j = j + 1)
+            {            
+                mat[i][j] = cont;
+                cont++;  
+            }             
+        }
+        for (i = 0; i < n; i = i + 1)
+        {
+            for (j = 0; j < m; j = j + 1)
+                printf("%3d", mat[i][j]);
+            putchar('\n');
+        }
+        printf("Imprimindo a transposta... \n");
+        transposta(mat, matTransp, m, n);
+        for (i = 0; i < n; i = i + 1)
+        {
+            for (j = 0; j < m; j = j + 1)
+                printf("%3d", matTransp[i][j]);
+            putchar('\n');
+        }
+    }
+   else printf("Matriz maior que o tamanho permitido -25\n");
+   return 0;
+}
