bbf276368786115a22b29fee94773833b9c72104
Fabio Tanikawa <79976718+Tonikawa@users.noreply.github.com>
2022-05-19 09:08:01 -0300

    Adicionando prints das threads

--- Projeto V.0.2/PROJv2.c
+++ Projeto V.0.2/PROJv2.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <omp.h>
-#include <time.h>
 
-long int T = 1000;
+
+long int T = 1000000000;
 
 void serie(double* globalVar);
 
@@ -11,15 +11,15 @@ int main(int argc, char* argv[]) {
 
   double globalVar = 0.0;
   int thread_count;
-  clock_t tempo;
+  
 
-  tempo = clock();
+  
   thread_count = strtol(argv[1], NULL, 10);
 # pragma omp parallel num_threads(thread_count)
   serie(&globalVar);
-  tempo = clock() - tempo;
-  printf("S = %.3f\n", globalVar);
-  printf("Tempo de execução = %1f", ((double)tempo)/((CLOCKS_PER_SEC/1000)));
+  
+  printf("termo(%ld) = %.3f\n", T, globalVar);
+
   
   return 0;
 }
@@ -29,12 +29,15 @@ void serie(double* globalVar) {
   int my_rank = omp_get_thread_num();
   int thread_count = omp_get_num_threads();
 
-  long int first_num = (my_rank * 200) + 1;
+  long int first_num = (my_rank * 200000000) + 1;
   
-  for (long int i = first_num; i < (first_num+200); i++) {
+  for (long int i = first_num; i < (first_num+200000000); i++) {
     my_result += 1/ (double) i;
   }
 
 # pragma omp critical
   *globalVar += my_result;
+
+  printf("Hello from thread %d of %d -> sum = %.3f\n", my_rank, thread_count, my_result);
+  
 }
