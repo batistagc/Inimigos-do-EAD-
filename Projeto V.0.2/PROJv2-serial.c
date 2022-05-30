8dfd76ed1f6727faf3278b4ca537a667f700e638
Fabio Tanikawa <79976718+Tonikawa@users.noreply.github.com>
2022-05-30 00:06:53 -0300

    Update PROJv2-serial.c

--- Projeto V.0.2/PROJv2-serial.c
+++ Projeto V.0.2/PROJv2-serial.c
@@ -1,20 +1,17 @@
 #include <stdlib.h>
-#include <time.h>
+
 #include <stdio.h>
 
 int main(void) {
-  clock_t tempo;
-  long int T = 1000;
+
+  long int T = 100000000;
   double soma = 0;
-  
-  tempo = clock();
   
   for (long int i = 1; i <= T; i++){
       soma += 1 / (double) i;
   }
-  tempo = clock() - tempo;
+
 
   printf("S = %.3f\n",soma);
-  printf("Tempo de execução = %1f", ((double)tempo)/((CLOCKS_PER_SEC/1000)));
   return 0;
 }
